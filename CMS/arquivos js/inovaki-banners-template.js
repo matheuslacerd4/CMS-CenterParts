7cbe86d78470b41c31ede1b2ad54957237860c01
Matheus Lacerda <matheuslacerda375@gmail.com>
2024-11-07 16:59:55 -0300

    entrar como - toolbar

--- CMS/arquivos js/inovaki-banners-template.js
+++ CMS/arquivos js/inovaki-banners-template.js
@@ -444,52 +444,58 @@ window.addEventListener("resize", checkScreenSize);
 // ------------------- btn toolbar ---------------------
 
 function newBtnToolbar() {
-  // botão "Trocar de cliente"
-  const filho = document.getElementById("impersonateButton");
-  const pai = filho.parentElement;
-  let formAberto = false;
+  vtexjs.checkout.getOrderForm().then(function (orderForm) {
+    const isLoggedIn = orderForm.userProfileId ? true : false;
 
-  pai.insertAdjacentHTML(
-    "beforeend",
-    `
+    const buttonText = isLoggedIn ? "Trocar de cliente" : "Entrar";
+    // botão "Trocar de cliente"
+
+    const filho = document.getElementById("impersonateButton");
+    const pai = filho.parentElement;
+    let formAberto = false;
+
+    pai.insertAdjacentHTML(
+      "beforeend",
+      `
     <span id="btnTrocaClient" style="cursor: pointer; height: 2.5rem; display: flex; align-items: center; justify-content: center;"
     class="cc-fr-ns cc-mt1 cc-mt0-ns cc-link cc-br2 cc-ba cc-pa0 cc-w-20-ns cc-ph3-ns cc-ba-ns cc-pv2-ns cc-db cc-dib-ns cc-tc cc-ttu cc-fw7 cc-f7 cc-dim cc-btn-change cc-silver">
-      <span>Trocar de cliente</span>
+      <span>${buttonText}</span>
     </span>
   `
-  );
+    );
 
-  filho.style.display = "none";
+    filho.style.display = "none";
 
-  document
-    .getElementById("btnTrocaClient")
-    .addEventListener("click", function () {
-      if (!formAberto) {
-        this.insertAdjacentHTML(
-          "afterend",
-          `
+    document
+      .getElementById("btnTrocaClient")
+      .addEventListener("click", function () {
+        if (!formAberto) {
+          this.insertAdjacentHTML(
+            "afterend",
+            `
       <div id="impersonateForm" style="position: absolute; top: 19%; left: 49%;">
         <input type="email" id="emailInput" placeholder="Digite o email do cliente" style="padding: 5px; width: 250px; height: 2.5rem;"/>
-        <button id="submitImpersonate" style="padding: 5px 10px; margin-left: 5px; background-color: #8dff6e; height: 2.5rem; border-color: #c1c4ca;">Trocar</button>
+        <button id="submitImpersonate" style="padding: 5px 10px; margin-left: 5px; background-color: #015ac2; height: 2.5rem; border-color: #c1c4ca; color: #fff;">Enviar</button>
       </div>
       `
-        );
+          );
 
-        // adiciona evento para o botão de submit
-        document
-          .getElementById("submitImpersonate")
-          .addEventListener("click", function () {
-            const email = document.getElementById("emailInput").value;
-            if (email) {
-              window.cc.impersonate(email);
-            }
-          });
-        formAberto = true;
-      } else {
-        document.getElementById("impersonateForm").remove();
-        formAberto = false;
-      }
-    });
+          // adiciona evento para o botão de submit
+          document
+            .getElementById("submitImpersonate")
+            .addEventListener("click", function () {
+              const email = document.getElementById("emailInput").value;
+              if (email) {
+                window.cc.impersonate(email);
+              }
+            });
+          formAberto = true;
+        } else {
+          document.getElementById("impersonateForm").remove();
+          formAberto = false;
+        }
+      });
+  });
 }
 
 // Cria um observador de mutações
